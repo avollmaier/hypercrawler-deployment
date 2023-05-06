a4a82fe56726f22d14acfc7b9d4dc45c23ca527d
avollmaier <alois.vollm@gmail.com>
2023-05-06 22:17:19 +0200

    start and stop endpoint with first messaging approach, new service

--- kubernetes/platform/create-cluster.sh
+++ kubernetes/platform/create-cluster.sh
@@ -4,12 +4,6 @@ echo "\n📦 Initializing Kubernetes cluster...\n"
 
 ctlptl create cluster minikube --registry=ctlptl-registry --kubernetes-version=v1.26.3
 
-echo "\n🔌 Enabling NGINX Ingress Controller...\n"
-
-minikube addons enable ingress
-
-sleep 30
-
 echo "\n📦 Deploying platform services..."
 
 kubectl apply -f services
@@ -28,5 +22,25 @@ kubectl wait \
   --for=condition=ready pod \
   --selector=db=hypercrawler-mongo \
   --timeout=180s
+
+echo "\n📦 Deploying RabbitMQ..."
+
+kubectl apply -f services/rabbitmq.yml
+
+sleep 5
+
+echo "\n⌛ Waiting for RabbitMQ to be deployed..."
+
+while [ $(kubectl get pod -l db=hypercrawler-rabbitmq | wc -l) -eq 0 ] ; do
+  sleep 5
+done
+
+echo "\n⌛ Waiting for RabbitMQ to be ready..."
+
+kubectl wait \
+  --for=condition=ready pod \
+  --selector=db=hypercrawler-rabbitmq \
+  --timeout=180s
+
 
 echo "\n⛵ Happy Sailing!\n"
